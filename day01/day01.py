ef4870a1f58ae69f6eafbf64392c32b564d9f40e
Ivan Jager <aij+git@mrph.org>
2022-12-01 17:24:16 -0600

    Day 1 part 2
    Also fixed bug where last group was ignored, even
    though it didn't affect my part 2 results.

--- day01/day01.py
+++ day01/day01.py
@@ -2,15 +2,21 @@
 
 import sys
 
-big=0
-cur=0
+def groups():
+    cur=0
+    for l in sys.stdin.readlines():
+        l = l.strip()
+        if l:
+            cur += int(l)
+        else:
+            yield cur
+            cur = 0
+    yield cur
 
-for l in sys.stdin.readlines():
-    l = l.strip()
-    if l:
-        cur += int(l)
-    else:
-        big = max(big, cur)
-        cur = 0
+all = list(groups())
 
-print(big)
+print(f"Part 1: {max(all)}")
+
+top3 = sorted(all)[-3:]
+print(repr(top3))
+print(f"Part two: Sum of top 3: {sum(top3)}")
