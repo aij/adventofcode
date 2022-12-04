d4fe5e01fe99c27579262f6b688693cb46659b3a
Ivan Jager <aij+git@mrph.org>
2022-12-04 13:31:50 -0600

    Day 4 part 2

--- day04/day04.py
+++ day04/day04.py
@@ -6,7 +6,16 @@ import sys
 def contains(a, b):
     return a[0] <= b[0] and a[1] >= b[1]
 
+# A fully before b
+def before(a,b):
+    return a[1] < b[0]
+
+
+def overlaps(a,b):
+    return not(before(a,b) or before(b,a))
+
 count = 0
+count2 = 0
 
 for l in sys.stdin.readlines():
     l = l.strip()
@@ -16,5 +25,9 @@ for l in sys.stdin.readlines():
     b = [int(x) for x in b]
     if (contains(a,b)) or contains(b,a):
         count += 1
+    if overlaps(a,b):
+        count2 +=1
+
 
 print(count)
+print(count2)
