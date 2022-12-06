682041f72bad590be86775ca5070bab8f9dffd25
Ivan Jager <aij+git@mrph.org>
2022-12-05 23:07:49 -0600

    Day 6 part 1 and 2

--- day06/day06.py
+++ day06/day06.py
@@ -11,3 +11,19 @@ lines = inp.split('\n') #[:-1]
 
 for l in lines:
     print(l)
+
+inp = inp.strip();
+
+for i in range(len(inp)-5):
+    mark = inp[i:i+4]
+    if len(set(mark)) == 4:
+        print(i+4)
+        break
+    print('not ' +mark)
+
+for i in range(len(inp)-5):
+    mark = inp[i:i+14]
+    if len(set(mark)) == len(mark):
+        print(i+14)
+        break
+    print('not ' +mark)
