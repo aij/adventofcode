222fc3c5c539136f0831d6902936a5cfb1517fc0
Ivan Jager <aij+git@mrph.org>
2022-12-06 22:55:15 -0600

    Day 7 prep

---
+++ day07/day07.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+import sys
+import os
+import re
+
+with open(sys.argv[1]) as f:
+    inp = f.read()
+
+lines = inp.split('\n') #[:-1]
+
+for l in lines:
+    print(l)
+
+#inp = inp.strip();
