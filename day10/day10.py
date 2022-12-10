dd98f8b44b473f194a634b8d8492e1bfa6ad6ce5
Ivan Jager <aij+git@mrph.org>
2022-12-10 00:14:54 -0600

    Day 10 partial cleanup

--- day10/day10.py
+++ day10/day10.py
@@ -1,16 +1,6 @@
 #!/usr/bin/env python3
 
-import sys
-import os
-import re
-
-def v2add(a, b):
-    return (a[0]+b[0], a[1]+b[1])
-
-def v2sub(a, b):
-    return (a[0]-b[0], a[1]-b[1])
-
-filename = 'example'
+#filename = 'example'
 filename = 'input'
 
 with open(filename) as f:
@@ -18,43 +8,35 @@ with open(filename) as f:
 
 lines = inp.rstrip('\n').split('\n')
 
-
 X = 1
 history = []
-instrs = []
-def cycle(i):
+def cycle():
     history.append(X)
-    instrs.append(i)
 
 for l in lines:
-    print(l)
     l = l.split(' ')
     if l[0] == 'noop':
-        cycle(l)
+        cycle()
     elif l[0] == 'addx':
         n = int(l[1])
-        cycle(l)
-        cycle(l)
+        cycle()
+        cycle()
         X += n
     else:
         assert False
 
-
 s = sum(history[i-1] * i for i in range(20, 221, 40))
-s = 0
-for i in range(20, 221, 40):
-    n = history[i-1] * i
-    print(f"{i} * {history[i-1]} = {n} running {instrs[i-1]}" )
-    s += n
 print(s)
 
 for (i,v) in enumerate(history):
     cycle = i+1
-    if i%40 == 0:
-        print()
     c = i%40 + 1
     if abs(c-(v+1)) <= 1:
         char = '#'
     else:
-        char = '.'
+        char = ' '
     print(char, end='')
+    if i%5 == 4: # Extra space between characters for readability (not to spec)
+        print('  ', end='')
+    if i%40 == 39:
+        print()
