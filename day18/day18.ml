96f671c6929334a2a7858fed4abc673b672c6310
Ivan Jager <aij+git@mrph.org>
2022-12-18 09:05:34 -0600

    Day 18 part 2, passing but ugly...

--- day18/day18.ml
+++ day18/day18.ml
@@ -1,4 +1,4 @@
-#!/usr/bin/env ocaml
+(*#!/usr/bin/env ocaml*)
 
 let filename = "example";;
 
@@ -26,5 +26,66 @@ let blobs : v3 list = List.map (fun s -> Scanf.sscanf s "%d,%d,%d" v3) lines;;
 let cube_surface v : int = List.fold_left (fun a n -> if List.mem n blobs then a else a+1) 0 (near v);;
 
 let surface_area : int = List.fold_left (fun a v -> a + (cube_surface v)) 0 blobs;;
+(* 4314 *)
 
-Printf.printf "Surface area: %d\n" surface_area;;
+Printf.printf "Total surface area: %d\n" surface_area;;
+flush stdout;;
+
+(* Do we need to worry about bigger trapped bubbles? *)
+(*
+let is_surrounded v = List.for_all (fun v -> List.mem v blobs) (near v)
+
+let cube_surface v : int = List.fold_left (fun a n -> if List.mem n blobs || is_surrounded n then a else a+1) 0 (near v);;
+
+
+let surface_area2 : int = List.fold_left (fun a v -> a + (cube_surface v)) 0 blobs;;
+
+Printf.printf "Bigger surface area: %d\n" surface_area;;
+ *)
+
+module V3 = struct
+  type t = int * int * int
+  let compare = compare
+
+end
+
+module V3Set = Set.Make(V3)
+
+(*let cube_edges = [(0,1,1); (0,1,-1); (0,-1,1); [0,-1,-1] ... ]*)
+let square_corners = [(1,1); (1,-1); (-1,1); (-1,-1)]
+let cube_edges = List.flatten [
+                     List.map (fun (x,y) -> (0,x,y)) square_corners;
+                     List.map (fun (x,y) -> (x,0,y)) square_corners;
+                     List.map (fun (x,y) -> (x,y,0)) square_corners;
+                   ]
+let cube_corners = List.flatten [
+                     List.map (fun (x,y) -> (1,x,y)) square_corners;
+                     List.map (fun (x,y) -> (-1,x,y)) square_corners;
+                     List.map (fun (x,y) -> (x,1,y)) square_corners;
+                     List.map (fun (x,y) -> (x,-1,y)) square_corners;
+                     List.map (fun (x,y) -> (x,y,1)) square_corners;
+                     List.map (fun (x,y) -> (x,y,-1)) square_corners;
+                     ]
+let cube_neighborhood = List.flatten [ cube_sides; cube_edges; (*cube_corners*) ];;
+let neighborhood v = List.map (v3add v) cube_neighborhood
+
+let list_count pred l = List.fold_left (fun a i -> if pred i then a+1 else a) 0 l
+let list_sum l = List.fold_left (fun a i -> a + i) 0 l
+
+(*let exterior_surface () : v3 list =*)
+  let hd = List.hd blobs
+  let leftmost = List.fold_left (fun ((x1,_,_) as a) ((x,_,_) as v) -> if x < x1 then v else a ) hd blobs
+  let start = v3add leftmost (-1,0,0)
+  let is_neighborhood v = not (List.mem v blobs) && List.exists (fun n -> List.mem n blobs) (neighborhood v)
+
+  let rec walk visited start = List.fold_left (fun a n -> if not(is_neighborhood n) || List.mem n a then a else walk (n::a) n) visited (near start)
+  let walked = walk [start] start
+  let surfaces_touching v = list_count (fun n -> List.mem n blobs) (near v)
+
+  let ext_counts = List.map surfaces_touching walked
+  let ext = list_sum ext_counts
+
+(*let ext = exterior_surface ()*)
+;;Printf.printf "Walked exterior: %d\n" (List.length walked )
+
+;;Printf.printf "Exterior surface area: %d\n" ( ext )
